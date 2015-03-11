6cf2985c341a7b2d0dab8a8a354621c13c254d1c
mike <mike@martini-corp.com>
2015-03-11 15:05:38 -0700

    unnecessary extra /

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
  * Scribe index.js
  */
-var src = process.env.SCRIBE_COV ? "/src-cov/" : "/src";
+var src = process.env.SCRIBE_COV ? "/src-cov" : "/src";
 module.exports = require(__dirname + src + "/scribe");
